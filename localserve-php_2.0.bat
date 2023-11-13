528412ad6f5a5a54bc38486fb042760e162b6e10
sharper <hi@sammyharper.com>
2023-11-13 10:45:21 -0600

    feat: add PHP v2.0

---
+++ localserve-php_2.0.bat
@@ -0,0 +1,31 @@
+@ECHO OFF
+:: v2.0
+:: to execute as expected, requires php > 5.4
+
+:start
+set choice=
+set /p choice= Open browser to view served website? [N]: 
+if not '%choice%'=='' set choice=%choice:~0,1%
+if '%choice%'=='Y' goto yes
+if '%choice%'=='y' goto yes
+if '%choice%'=='N' goto no
+IF '%choice%'=='n' goto no
+if '%choice%'=='' goto no
+echo "%choice%" is not valid
+echo.
+goto start
+
+:no
+echo Skipped...
+clear
+GOTO
+
+:yes
+echo Opening browser...
+start "" http://localhost:8000
+
+:startServer
+ECHO starting local server at
+ECHO %cd%
+ECHO with php (localhost:8000)
+php -S localhost:8000
